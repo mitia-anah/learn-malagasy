c0c69f14dc2fd6b8b424b9868bb1e1d9150ca33c
student <Rosny>
2021-04-28 14:21:37 +0300

    added component SectionHeading

---
+++ src/components/SectionHeading/SectionHeading.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {View} from 'react-native';
+
+function SectionHeading() {
+  return (
+    <View>
+      <Text h3></Text>
+    </View>
+  );
+}
+export default SectionHeading;
---
+++ src/components/SectionHeading/SectionHeading.stories.js
@@ -0,0 +1,7 @@
+import {storiesOf} from '@storybook/react-native';
+import React from 'react';
+import {View} from 'react-native';
+
+import SectionHeading from './SectionHeading';
+
+storiesOf('SectionHeading', module).addDecorator(story => <View></View>);

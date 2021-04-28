7be9c5f14c38fe1ff4896eca590c493c3baa8172
student <Rosny>
2021-04-28 12:20:05 +0300

    added (add, seen, learnt, back, nightMode) buttons

--- src/components/ToolButton/ToolButton.js
+++ src/components/ToolButton/ToolButton.js
@@ -1,11 +1,32 @@
 import React from 'react';
-import {Text, TouchableOpacity, StyleSheet, Button} from 'react-native';
+import {Text, TouchableOpacity, StyleSheet, Button, View} from 'react-native';
+import {Icon} from 'react-native-elements/dist/icons/Icon';
 // import {Icon} from 'react-native-element
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    display: 'flex',
+  },
+  button: {
+    backgroundColor: '#06B6D4',
+    width: 40,
+    borderRadius: 100,
+    height: 40,
+  },
+  icon: {
+    textAlign: 'center',
+    paddingTop: 6,
+  },
+});
 
-function ToolButton() {
-  return <TouchableOpacity></TouchableOpacity>;
+function ToolButton({name}) {
+  return (
+    <TouchableOpacity onPress={() => {}}>
+      <View style={styles.button}>
+        <Icon style={styles.icon} name={name} color="#fff" type="material" />
+      </View>
+    </TouchableOpacity>
+  );
 }
 
 export default ToolButton;
--- storybook/storyLoader.js
+++ storybook/storyLoader.js
@@ -5,22 +5,24 @@
 
 function loadStories() {
   require('../src/components/ActionButton/ActionButton.stories');
-  require('../src/components/Example/Example.stories');
+  // require('../src/components/Example/Example.stories');
   require('../src/components/NextButton/NextButton.stories');
-  require('../src/components/Phrase/Rosny.stories');
+  // require('../src/components/Phrase/Rosny.stories');
   require('../src/components/PhraseTextarea/PhraseTextarea.stories');
-  require('./stories/Button/Button.stories');
-  require('./stories/Welcome/Welcome.stories');
+  require('../src/components/ToolButton/ToolButton.stories');
+  // require('./stories/Button/Button.stories');
+  // require('./stories/Welcome/Welcome.stories');
 }
 
 const stories = [
   '../src/components/ActionButton/ActionButton.stories',
-  '../src/components/Example/Example.stories',
+  // '../src/components/Example/Example.stories',
   '../src/components/NextButton/NextButton.stories',
-  '../src/components/Phrase/Rosny.stories',
+  // '../src/components/Phrase/Rosny.stories',
   '../src/components/PhraseTextarea/PhraseTextarea.stories',
-  './stories/Button/Button.stories',
-  './stories/Welcome/Welcome.stories',
+  '../src/components/ToolButton/ToolButton.stories',
+  // './stories/Button/Button.stories',
+  // './stories/Welcome/Welcome.stories',
 ];
 
 module.exports = {

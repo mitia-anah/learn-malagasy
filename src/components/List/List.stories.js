896405272691a31c0408ceda51b77af54623243c
student <Rosny>
2021-04-30 11:06:04 +0300

    added list component and stories with styles

---
+++ src/components/List/List.stories.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import {storiesOf} from '@storybook/react-native';
+import {View} from 'react-native';
+import List from './List';
+
+storiesOf('List', module)
+  .addDecorator(story => (
+    <View
+      style={{
+        backgroundColor: '#E5E5E5',
+        paddingStart: 10,
+        paddingEnd: 10,
+      }}>
+      {story()}
+    </View>
+  ))
+  .add('Displays list of number of list items,', () => (
+    <List
+      title="Learn"
+      name="arrow-forward"
+      color="#06B6D4"
+      type="material"
+      headingTitle="Select a category:"></List>
+  ));
